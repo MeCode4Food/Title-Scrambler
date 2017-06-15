33214db37d749fb23159e8b1dd17735f3a5ced7e
Chick <cheecken0@hotmail.com>
2017-06-15 23:09:13 +0800

    First commit

---
+++ Title Scrambler v0.2/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Title_Scrambler_v0._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_browse = new System.Windows.Forms.Button();
+            this.button_execute = new System.Windows.Forms.Button();
+            this.textBox_extension = new System.Windows.Forms.TextBox();
+            this.textBox_directory = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_browse
+            // 
+            this.button_browse.Location = new System.Drawing.Point(305, 44);
+            this.button_browse.Name = "button_browse";
+            this.button_browse.Size = new System.Drawing.Size(75, 23);
+            this.button_browse.TabIndex = 0;
+            this.button_browse.Text = "Browse";
+            this.button_browse.UseVisualStyleBackColor = true;
+            this.button_browse.Click += new System.EventHandler(this.button_browse_Click);
+            // 
+            // button_execute
+            // 
+            this.button_execute.Location = new System.Drawing.Point(305, 98);
+            this.button_execute.Name = "button_execute";
+            this.button_execute.Size = new System.Drawing.Size(75, 23);
+            this.button_execute.TabIndex = 1;
+            this.button_execute.Text = "Scramble!";
+            this.button_execute.UseVisualStyleBackColor = true;
+            this.button_execute.Click += new System.EventHandler(this.button_execute_Click);
+            // 
+            // textBox_extension
+            // 
+            this.textBox_extension.Location = new System.Drawing.Point(27, 101);
+            this.textBox_extension.Name = "textBox_extension";
+            this.textBox_extension.Size = new System.Drawing.Size(254, 20);
+            this.textBox_extension.TabIndex = 2;
+            // 
+            // textBox_directory
+            // 
+            this.textBox_directory.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.textBox_directory.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox_directory.Location = new System.Drawing.Point(27, 44);
+            this.textBox_directory.Name = "textBox_directory";
+            this.textBox_directory.Size = new System.Drawing.Size(254, 20);
+            this.textBox_directory.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Directory";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Extension (.mp3, .txt etc)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 164);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_directory);
+            this.Controls.Add(this.textBox_extension);
+            this.Controls.Add(this.button_execute);
+            this.Controls.Add(this.button_browse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_browse;
+        private System.Windows.Forms.Button button_execute;
+        private System.Windows.Forms.TextBox textBox_extension;
+        private System.Windows.Forms.TextBox textBox_directory;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Title Scrambler v0.2/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.IO;
+using Title_Scrambler_v0._2.Properties;
+
+namespace Title_Scrambler_v0._2
+{
+    public partial class Form1 : Form
+    {
+        string folderPath = "";
+        public Form1()
+        {
+            InitializeComponent();
+            textBox_directory.Text = Properties.Settings.Default.temp_path;
+            textBox_extension.Text = Properties.Settings.Default.temp_extension;
+            folderPath = Properties.Settings.Default.temp_path;
+            this.FormClosing += Form1_FormClosing1;
+        }
+
+        private void Form1_FormClosing1(object sender, FormClosingEventArgs e)
+        {
+            
+            Properties.Settings.Default.temp_path = folderPath;
+            Properties.Settings.Default.temp_extension = textBox_extension.Text;
+            Properties.Settings.Default.Save();
+            
+        }
+
+        private void button_browse_Click(object sender, EventArgs e)
+        {
+
+            FolderBrowserDialog FBD1 = new FolderBrowserDialog();
+            if (FBD1.ShowDialog() == DialogResult.OK)
+            {
+                folderPath = FBD1.SelectedPath;
+            }
+            textBox_directory.Text = folderPath;
+            Properties.Settings.Default.temp_path = folderPath;
+            Properties.Settings.Default.Save();
+
+        }
+
+        private void button_execute_Click(object sender, EventArgs e)
+        {
+            if (folderPath == "")
+            {
+                MessageBox.Show("Please select your directory.");
+            }
+            else if(textBox_extension.Text == "")
+            {
+                MessageBox.Show("Please select the extension to be used.");
+            }
+            else
+            {
+                //collect array(list) of titles
+                string[] Directories = Directory.GetFiles(folderPath, "*" + textBox_extension.Text);
+
+                //Directory dump
+
+                System.Console.WriteLine("Directory List:");
+                foreach (string element in Directories)
+                {
+                    System.Console.WriteLine(element);
+                }
+                System.Console.WriteLine();
+
+                int Length = Directories.Length;
+                ArrayList TitleList = new ArrayList();
+                ArrayList TempTitleList = new ArrayList();
+                ArrayList ScrambledList = new ArrayList();
+
+                for (int i = 0; i < Length; i++)
+                {
+                    TitleList.Add(Directories[i]);
+                    TempTitleList.Add(Directories[i]);
+                }
+
+                //TitleList and TempTitleList dump
+
+                System.Console.WriteLine("Title List:");
+                foreach (string element in TitleList)
+                {
+                    System.Console.WriteLine(element);
+                }
+                System.Console.WriteLine();
+
+                //Random seed for scrambling
+                Random r = new Random();
+
+                //Scramble
+                for (int i = 0; i < Length; i++)
+                {
+                    int rInt = r.Next(0, TempTitleList.Count);
+                    ScrambledList.Add(TempTitleList[rInt]);
+                    TempTitleList.RemoveAt(rInt);
+                }
+
+                //TitleList and TempTitleList dump
+                System.Console.WriteLine("Original Title List:");
+                foreach (string element in TempTitleList)
+                {
+                    System.Console.WriteLine(element);
+                }
+                System.Console.WriteLine();
+
+                //Scrambled Title List dump
+                System.Console.WriteLine("Scrambled Title List:");
+                foreach (string element in ScrambledList)
+                {
+                    System.Console.WriteLine(element);
+                }
+                System.Console.WriteLine();
+
+                //Rename
+                
+                for(int i = 0; i < Length; i++)
+                {
+                    if (File.Exists((string)TitleList[i]) && (TitleList[i] != ScrambledList[i]))
+                    {
+                        //copy original to temp
+                        try
+                        {
+                            File.Copy((string)TitleList[i], folderPath + "\\temp", true);
+                        }catch
+                        {
+                            MessageBox.Show("Can't copy from " +(string)TitleList[i] + " to " + folderPath + "\\temp");
+                        }
+                        
+                        System.Console.WriteLine("Copying from "+ (string)TitleList[i] + " to " + folderPath + "\\temp");
+
+                        //copy scrambled to original, overwriting
+                        try
+                        {
+                            File.Copy((string)ScrambledList[i], (string)TitleList[i], true);
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Can't copy from " + (string)ScrambledList[i] + " to " + (string)TitleList[i]);
+                        }
+                        System.Console.WriteLine("Copying from " + (string)ScrambledList[i] + " to " + (string)TitleList[i]);
+
+                        //copy temp to scrambled
+                        try
+                        {
+                            File.Copy(folderPath + "\\temp", (string)ScrambledList[i], true);
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Can't copy from " + folderPath + "\\temp" + " to " + (string)ScrambledList[i]);
+                        }
+                        System.Console.WriteLine("Copying from " + folderPath + "\\temp" + " to " +  (string)ScrambledList[i]);
+
+                        //delete temp
+                        try
+                        {
+                            File.Delete(folderPath + "\\temp");
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Can't delete from " + folderPath + "\\temp");
+                        }
+                        System.Console.WriteLine("Deleting from " + folderPath + "\\temp");
+
+                    }
+                    System.Console.WriteLine(folderPath + "\\temp");
+
+                }
+                MessageBox.Show("Mischief Managed.");
+
+
+            }
+
+        }
+    }
+}
